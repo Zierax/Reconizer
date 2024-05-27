6797347b50ac5dc8eb2bb0f4c2d54bd286a88f81
Ziad <153237520+Zierax@users.noreply.github.com>
2024-05-27 00:12:15 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,9 @@
 
 # Install dependencies
 echo -e "\033[1;32m[+] Installing dependencies...\033[0m"
+go install github.com/tomnomnom/httprope@latest
+go install github.com/tomnomnom/waybackurls@latest
+go install github.com/tomnomnom/gau@latest
 sudo apt update
 sudo apt install -y amass subfinder assetfinder sublist3r dnsrecon dnsx massdns masscan nmap eyewitness dirsearch nikto
 
